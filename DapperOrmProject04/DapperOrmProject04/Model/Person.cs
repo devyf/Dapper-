22cc58b08f6d8d845acfe42b33474678005aa019
devyf <hyfmailsave@163.com>
2020-08-16 16:31:17 +0800

    04-提交Dapper数据库删除功能代码

---
+++ DapperOrmProject04/DapperOrmProject04/Model/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperOrmProject.Model
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime CreateTime { get; set; }
+    }
+}
---
+++ DapperOrmProject04/DapperOrmProject04/Service/PersonService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperOrmProject.Model;
+
+namespace DapperOrmProject.Service
+{
+    public class PersonService
+    {
+        /// <summary>
+        /// 根据传入的ID进行数据删除
+        /// </summary>
+        /// <param name="person"></param>
+        public bool DeleteDataById(int id)
+        {
+            //IDbConnection由于Dapper ORM的操作实际上是对IDbConnection类的扩展，所有的方法都是该类的扩展方法。
+            using (IDbConnection db = new SqlConnection(DBHelper.ConnString))
+            {
+                string delSql = "delete from person where id = @ID";
+                //类中的属性一致
+                int exeRes = db.Execute(delSql, new { ID = id});
+
+                return exeRes > 0;
+            }
+        }
+    }
+}
